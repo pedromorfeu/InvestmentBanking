f31cfd701cc02fe3508f2bddfcaed31a2332301e
Pedro Henrique Mano Fernandes <pedromorfeu@gmail.com>
2016-08-29 12:06:28 +0200

    Test TradeId

--- tests.py
+++ tests.py
@@ -3,13 +3,20 @@ from FpML import Leg, fpml
 import os
 import sys
 
-x = Leg(2, 3)
-print(x)
-print(x.ccy)
-print(x.amount)
+# x = Leg(2, 3)
+# print(x)
+# print(x.ccy)
+# print(x.amount)
 
-print(os.getcwd())
+trade = fpml.Trade(fpml.TradeHeader(fpml.PartyTradeIdentifier()))
+print(trade)
+print(type(trade.tradeHeader.partyTradeIdentifier))
+print(trade.tradeHeader.partyTradeIdentifier)
 
-x = fpml.ValuationDocument()
-print(x)
+trade.tradeHeader.partyTradeIdentifier.tradeId = fpml.TradeId()
+print(type(trade.tradeHeader.partyTradeIdentifier.tradeId.id))
+print(trade.tradeHeader.partyTradeIdentifier.tradeId.id)
 
+x = fpml.TradeId("EUR", tradeIdScheme = "http://www.citi.com/fx/trade-id")
+# x.tradeIdScheme = "http://www.citi.com/fx/trade-id"
+print(x.toxml("utf-8", element_name='TradeId'))
