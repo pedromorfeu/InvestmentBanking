06c966f036a5ebf313a87a070af45a02f258db07
Pedro <pedromorfeu@gmail.com>
2016-08-30 18:06:25 +0200

    Egg FpML 0.0.3

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="FpML",
+    version="0.0.3",
+    description="FpML",
+    packages=["FpML"]
+)
+# find_packages looks for all packages under the current folder
